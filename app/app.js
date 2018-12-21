75764c940c269e38f8ebf2854f2547b2116fbdb8
Unknown <matthew.alexander.wong@gmail.com>
2018-12-21 10:36:11 -0700

    Use express to run server on localHost 3000!

---
+++ app/app.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+var bodyParser = require("body-parser");
+
+const port = 3000
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
