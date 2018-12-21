8934f39a1990c25c16134929723563a4e77ed612
Unknown <matthew.alexander.wong@gmail.com>
2018-12-21 10:41:24 -0700

    Use public folder to get styles to work.

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ var app = express();
 var path = require("path");
 var bodyParser = require("body-parser");
 
+app.use(express.static(__dirname + '/public'));
+
 const port = 3000
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + "/index.html"));
