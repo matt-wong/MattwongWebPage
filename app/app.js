b4be7305dcde2d7e6366758f2eef9ea2d00756f0
Unknown <matthew.alexander.wong@gmail.com>
2018-12-29 15:28:51 -0700

    Add random profile pictures with Lorem quotes.

--- app/app.js
+++ app/app.js
@@ -1,13 +1,31 @@
 var express = require("express");
 var app = express();
 var path = require("path");
+var request = require("request");
 var bodyParser = require("body-parser");
+var faker = require("faker")
 
 app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
 
 const port = 3000
+
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.get('/learnMore', function(req, res){
+//Random Photos with Quotes
+arrayOfPeopleImages = [];
+for (var i=0; i<3; i++){
+    let personImage = faker.image.avatar();
+    console.log(personImage);
+    arrayOfPeopleImages[i] = personImage;
+}
+
+console.log(faker.company.catchPhrase());
+res.render("learnMore", {arrayOfPeopleImages: arrayOfPeopleImages});
+
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
